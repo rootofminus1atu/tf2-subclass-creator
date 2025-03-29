d6233e585ecfa9aa76846c4f17b1b468c190b2a5
RootOfMinus1ATU <s00240122@atu.ie>
2025-03-29 23:20:49 +0000

    Update environment.development.ts

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
-// const API_URI = 'http://localhost:3000/api/v1'
-const API_URI = 'https://tf2-subclass-creator.onrender.com/api/v1'
+const API_URI = 'http://localhost:3000/api/v1'
+// const API_URI = 'https://tf2-subclass-creator.onrender.com/api/v1'
 // const SHUTTLE_URI = 'https://servicenexus-hhq1.shuttle.app/tf2sc'
 const AWS_URI = 'https://p7w81i8dr1.execute-api.eu-west-1.amazonaws.com/v1/api'
 const ANALYTICS_API = 'G-4M85EFFZFC'
